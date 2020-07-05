2cfa0153058fb5f85f5f2da1e310eeb1483184ed
Swarup Hegde <swaruphegde@gmail.com>
2020-07-05 19:05:45 +0530

    better run time

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,19 @@
 def fib(n):
+    f = []
+    f[0], f[1] = 0, 1
     if n == 1:
         return 1
     if n == 0:
         return 0
-    else:
-        return fib(n-1) + fib(n-2)
+    for i in range(2, n):
+        f.append(f[i-1] + f[i-2])
+    return f
 
 
         '''
-            O(2^n)
+            O(2^n) => :C 
+            O(N) => :D 
+            N = 1000
+            1000ms = 1s
+            2^1000 
         '''
